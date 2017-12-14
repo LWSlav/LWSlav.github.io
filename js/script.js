74d733fbe477e35c9fe3a37f3f3ad2574b51ad3e
LWSlav <33983022+LWSlav@users.noreply.github.com>
2017-12-14 11:31:11 +0600

    18lesson

--- js/script.js
+++ js/script.js
@@ -1,10 +1,39 @@
 $(document).ready(function() {
-	$('.tab').click(function() {
-		var content = $(this).attr('href');
-		console.log(content);
-		$('.content').hide(300);
-		$(content).show(300);
-		
-	});
-});
+	$('#form').validate({
+		rules: {
+			name: {
+				required : true,
+				minlength : 3
+			},
 
+			email: {
+				required : true,
+				minlength : 6,
+				email : true
+			},
+
+			password: {
+				required : true,
+				minlength : 4
+			}
+		},
+
+		messages: {
+			name: {
+				required : 'Обязательное поле',
+				minlength : 'Не менее 3 символов'
+			},
+
+			email: {
+				required : 'Обязательное поле',
+				minlength : 'Не менее 6 символов',
+				email : 'Некорректный email'
+			},
+
+			password: {
+				required : 'Обязательное поле',
+				minlength : 'Не менее 4 символов'
+			}
+		},
+	})
+})
