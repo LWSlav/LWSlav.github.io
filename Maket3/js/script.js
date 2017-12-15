ea7e6109bd413ca21616ae3db59a04dc3652bcb2
LWSlav <33983022+LWSlav@users.noreply.github.com>
2017-12-15 11:15:31 +0600

    Maket3

---
+++ Maket3/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('.first-screen').parallax({imageSrc: 'img/bg.jpg'});
+	
+  // Center all the things!
+  var rellax = new Rellax('.rellax', {
+    center: true
+  });
+
+})
