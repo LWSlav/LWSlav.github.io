20cd00d042a7fa4dd661e48342c9af86b0dc7824
LWSlav <33983022+LWSlav@users.noreply.github.com>
2017-12-15 11:13:49 +0600

    Maket2

---
+++ Maket2/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('.own-img').parallax({imageSrc: 'img/z-girl.jpg'});
+	$('.two-img').parallax({imageSrc: 'img/e-girl.jpg'});
+	$('.three-img').parallax({imageSrc: 'img/t-girl.jpg'});
+	$('.fore-img').parallax({imageSrc: 'img/s-girl.jpg'});
+})
