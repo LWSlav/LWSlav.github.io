441b46802a5a53bb127ffed88f55c9e1c8dd477c
LWSlav <33983022+LWSlav@users.noreply.github.com>
2018-02-18 13:59:34 +0600

    Add files via upload

---
+++ kovanye/smart.php
@@ -0,0 +1,34 @@
+<?php 
+$name = htmlspecialchars($_POST['name']);
+$phone = htmlspecialchars($_POST['phone']);
+$email = htmlspecialchars($_POST['email']);
+$form = htmlspecialchars($_POST['form']);
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'vlokosov@list.ru';                 // SMTP username
+$mail->Password = '19981801PHT13q';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+$mail->setFrom('vlokosov@list.ru', 'Вячеслав');
+$mail->addAddress('Slaw.lokosoff@yandex.ru', 'Заказчик');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = 'Заявка с формы';
+$mail->Body    = 'Это имя отправителя: '.$name.' , '.'Это телефон отправителя '.$phone.' , '.'Это email отправителя '.$email;
+$mail->AltBody = 'Это сообщение в формате plain text';
+if(!$mail->send()) {
+    echo 'Не ушло(';
+}else{
+	header("Location: index.html");
+}
+?>
