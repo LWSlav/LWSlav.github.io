86366c3f203a1de359008e8e045e9c901d357aea
LWSlav <33983022+LWSlav@users.noreply.github.com>
2018-02-23 15:49:35 +0600

    Add files via upload

--- kovanye/js/script.js
+++ kovanye/js/script.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
             top = $(id).offset().top;
          
         //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
+    $('body,html').animate({scrollTop: top}, 1500);});
+    $('.first').parallax({imageSrc: 'img/23.jpg'});
+    $('.form-section').parallax({imageSrc: 'img/gates-of-heaven-clipart-18.jpg'});
+    new WOW().init();
 })  
