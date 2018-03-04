616bb7041ded69fce09bf0e646eb3014687d4738
LWSlav <33983022+LWSlav@users.noreply.github.com>
2018-03-04 23:03:15 +0600

    Add files via upload

---
+++ event/js/script.js
@@ -0,0 +1,45 @@
+function formClick(form) {
+	$(form.name).css('border', '1px solid #5C5C5C');
+	$(form.tel).css('border', '1px solid #5C5C5C');
+	var name = form.name.value;
+	var phone = form.tel.value;
+	
+	if (name.length < 2) {
+		$(form.name).css('border', '2px solid #E57474');
+		return false
+	}
+	else if (phone.length < 6) {
+		$(form.tel).css('border', '2px solid #E57474');
+		return false
+	}
+	else{
+		document.getElementById('form').reset();
+		document.getElementById('form-popup').reset();
+	}
+};
+
+jQuery(document).ready(function() {
+	 $('.project-slide').slick({
+	 	autoplay: true,
+	 	autoplaySpeed: 1000,
+	 	speed: 1500,
+	 	arrows: true,
+	 	nextArrow: $('.arrow-slide-next'), 
+	 	prevArrow: $('.arrow-slide-prev'), 
+	 });
+
+	 $('.clous, .X-clous').click(function() {
+	 	$('.clous').hide(500);
+	 	$('.popup').css('top', '-1000px');
+	 });
+
+	 $('.button-1, .Zakaz').click(function() {
+	 	$('.clous').show(500);
+	 	$('.popup').css('top', '100px');
+	 });
+
+	 $('.navfixed').click(function() {
+	 	$('.navfixed span').toggleClass('navActive');
+	 	$('nav').toggleClass('nav-manuActive');
+	 });
+});
